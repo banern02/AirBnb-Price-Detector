4bf535ce566d02c61fee93bc144feaa99ebf5c58
Sagnik Mondal <sagnik.mondal.20@gmail.com>
2023-05-06 19:44:32 -0400

    changes in yaml

--- airbnbprice/lib/main.dart
+++ airbnbprice/lib/main.dart
@@ -9,7 +9,7 @@ import 'logo.dart';
 
 final List<TextEditingController> _controllers = List.generate(11, (index) => TextEditingController());
 
-final List<String> room_list = ['Select an option', 'Entire room/apt', 'Hotel room', 'Private room', 'Shared room'];
+final List<String> room_list = ['Select an option', 'Entire home/apt', 'Hotel room', 'Private room', 'Shared room'];
 final List<String> Neighbourhood_group_list = ['Select an option', 'Brooklyn', 'Manhattan', 'Queens', 'Bronx', 'Staten Island'];
 final List<String> Neighbourhood_brooklyn = ['Select an option','Bath Beach','Bay Ridge','Bedford-Stuyvesant','Bensonhurst','Bergen Beach','Boerum Hill','Borough Park','Brighton Beach','Brooklyn Heights','Brownsville','Bushwick','Canarsie','Carroll Gardens','Clinton Hill','Cobble Hill','Columbia St','Coney Island','Crown Heights','Cypress Hills','DUMBO','Downtown Brooklyn','Dyker Heights','East Flatbush','East New York','Flatbush','Flatlands','Fort Greene','Fort Hamilton','Gerritsen Beach','Gowanus','Gravesend','Greenpoint','Kensington','Manhattan Beach','Midwood','Mill Basin','Navy Yard','Park Slope','Prospect Heights','Prospect-Lefferts Gardens','Red Hook','Sea Gate','Sheepshead Bay','South Slope','Sunset Park','Vinegar Hill','Williamsburg','Windsor Terrace'];
 final List<String> Neighbourhood_manhattan = ['Select an option','Battery Park City','Chelsea','Chinatown','Civic Center','East Harlem','East Village','Financial District','Flatiron District','Gramercy','Greenwich Village','Harlem','Hell\'s Kitchen','Inwood','Kips Bay','Little Italy','Lower East Side','Marble Hill','Midtown','Morningside Heights','Murray Hill','NoHo','Nolita','Roosevelt Island','SoHo','Stuyvesant Town','Theater District','Tribeca','Two Bridges','Upper East Side','Upper West Side','Washington Heights','West Village'];
@@ -18,6 +18,24 @@ final List<String> Neighbourhood_bronx = ['Select an option','Allerton','Bayches
 final List<String> Neighbourhood_staten = ['Select an option','Arden Heights','Arrochar','Bull\'s Head','Castleton Corners','Chelsea, Staten Island','Clifton','Concord','Dongan Hills','Eltingville','Emerson Hill','Fort Wadsworth','Graniteville','Grant City','Great Kills','Grymes Hill','Howland Hook','Huguenot','Lighthouse Hill','Mariners Harbor','Midland Beach','New Brighton','New Dorp','New Dorp Beach','New Springville','Oakwood','Port Richmond','Prince\'s Bay','Randall Manor','Richmondtown','Rosebank','Shore Acres','Silver Lake','South Beach','St. George','Stapleton','Todt Hill','Tompkinsville','Tottenville','West Brighton','Westerleigh','Willowbrook','Woodrow'];
 List<String> Neighbourhood_dropdown = ['Select an option'];
 var predValue = "";
+RangeValues _values2 = RangeValues(40.50029, 40.91063);
+RangeValues _values3 = RangeValues(-74.24985, -73.71086);
+
+final Map Neighbourhood = {'Sunset Park': 0, 'Upper West Side': 1,  'Bedford-Stuyvesant': 2,  'Midtown': 3,  'East Harlem': 4,  'South Slope': 5,  'Williamsburg': 6,  'Fort Greene': 7,  "Hell's Kitchen": 8,  'Harlem': 9,  'Long Island City': 10,  'East Village': 11,  'Lower East Side': 12,  'West Village': 13,  'Greenpoint': 14,  'East Flatbush': 15,  'Clinton Hill': 16,  'Chelsea': 17,  'Prospect Heights': 18,  'Eastchester': 19,  'Crown Heights': 20,  'Boerum Hill': 21,  'Washington Heights': 22,  'Upper East Side': 23,  'Nolita': 24,  'Kips Bay': 25,  'Sunnyside': 26,
+    'Carroll Gardens': 27,  'St. George': 28,  'Park Slope': 29,  'Gowanus': 30,  'Ridgewood': 31,  'Morningside Heights': 32,  'Rockaway Beach': 33,  'Ditmars Steinway': 34,  'Middle Village': 35,  'Chinatown': 36,  'Greenwich Village': 37,  'Kingsbridge': 38,  'Jamaica': 39,  'Astoria': 40,  'Brooklyn Heights': 41,  'Flatiron District': 42,  'Windsor Terrace': 43,  'Tribeca': 44,  'Bushwick': 45,  'Forest Hills': 46,  'Murray Hill': 47,  'Stuyvesant Town': 48,  'Sheepshead Bay': 49,  'Arrochar': 50,  'East New York': 51,
+    'Emerson Hill': 52,  'Bensonhurst': 53,  'Theater District': 54,  'University Heights': 55,  'Rego Park': 56,  'Kensington': 57,  'Shore Acres': 58,  'Woodside': 59,  'Allerton': 60,  'Richmond Hill': 61,  'Prospect-Lefferts Gardens': 62,  'Elmhurst': 63,  'Gramercy': 64,  'SoHo': 65,  'Little Italy': 66,  'Bayside': 67,  'Brighton Beach': 68,  'New Springville': 69,  'Mott Haven': 70,  'Jackson Heights': 71,  'Maspeth': 72,  'Inwood': 73,  'Spuyten Duyvil': 74,  'Briarwood': 75,  'Battery Park City': 76,  'Flushing': 77,  'Two Bridges': 78,  'Gravesend': 79,  'Fort Hamilton': 80,
+    'Cypress Hills': 81,  'Mariners Harbor': 82,  'East Elmhurst': 83,  'Downtown Brooklyn': 84,  'Cobble Hill': 85,  'Concord': 86,  'Tottenville': 87,  'Flatbush': 88,  'Melrose': 89,  'NoHo': 90,  'Columbia St': 91,  'College Point': 92,  'Vinegar Hill': 93,  'Concourse': 94,  'DUMBO': 95,  'City Island': 96,  'Red Hook': 97,  'Bay Ridge': 98,  'Longwood': 99,  'Financial District': 100,  'Canarsie': 101,  'Port Morris': 102,  'Flatlands': 103,  'Arverne': 104,  'Glendale': 105,  'Williamsbridge': 106,  'Soundview': 107,  'Woodhaven': 108,  'Parkchester': 109,  'Bronxdale': 110,
+  'Ozone Park': 111,  'Borough Park': 112,  'Bay Terrace': 113,  'Claremont Village': 114,  'Fordham': 115,  'Clifton': 116,  'Concourse Village': 117,  'Huguenot': 118,  'Brownsville': 119,  'Mount Hope': 120,  'Wakefield': 121,  'Midwood': 122,  'Navy Yard': 123,  'Fieldston': 124,  'Civic Center': 125,  'Norwood': 126,  'Lighthouse Hill': 127,  'Clason Point': 128,  'Unionport': 129,  'Randall Manor': 130,  'Kew Gardens Hills': 131,  'Jamaica Estates': 132,  'Kew Gardens': 133,  'Bellerose': 134,  'Fresh Meadows': 135,  'Morris Park': 136,  'South Ozone Park': 137,  'East Morrisania': 138,
+  'Far Rockaway': 139,  'Springfield Gardens': 140,  'Tremont': 141,  'Corona': 142,  'West Brighton': 143,  'Manhattan Beach': 144,  'Dongan Hills': 145,  'Marble Hill': 146,  'Roosevelt Island': 147,  'Laurelton': 148,  'Hunts Point': 149,  'Queens Village': 150,  'Great Kills': 151,  'Howard Beach': 152,  'Silver Lake': 153,  'Riverdale': 154,  'Grymes Hill': 155,  'Holliswood': 156,  'Tompkinsville': 157,  'Castleton Corners': 158,  'St. Albans': 159,  'New Brighton': 160,  'Edgemere': 161,  'Pelham Gardens': 162, 'Baychester': 163,  'Sea Gate': 164,  'Bergen Beach': 165,  'Stapleton': 166,
+    'Cambria Heights': 167,  'Woodlawn': 168,  'Richmondtown': 169,  'Olinville': 170,  'Dyker Heights': 171,  'Throgs Neck': 172,  'Coney Island': 173,  'Rosedale': 174,  'Highbridge': 175,  'Howland Hook': 176,  'Pelham Bay': 177,  'Bath Beach': 178,  'South Beach': 179,  'Midland Beach': 180,  'Oakwood': 181,  'Eltingville': 182,  'Schuylerville': 183,  'Whitestone': 184,  'Edenwald': 185,  'Mount Eden': 186,  'Neponsit': 187,  'Morris Heights': 188,  'Westchester Square': 189,  'Hollis': 190,  'Van Nest': 191,  'Rosebank': 192,  'Grant City': 193,  'Port Richmond': 194,  'North Riverdale': 195,  'Country Club': 196,
+    'Morrisania': 197,  'Belmont': 198,  'Bayswater': 199,  'Willowbrook': 200,  'New Dorp Beach': 201,  'Douglaston': 202,  'Todt Hill': 203,  'Mill Basin': 204,  'Little Neck': 205,  'Arden Heights': 206,  'Co-op City': 207,  'Jamaica Hills': 208,  "Prince's Bay": 209,  "Bull's Head": 210,  'Castle Hill': 211,  'West Farms': 212,  'Belle Harbor': 213,  'Westerleigh': 214,  'Chelsea, Staten Island': 215,  'Gerritsen Beach': 216,  'Breezy Point': 217,  'Woodrow': 218,  'Graniteville': 219,  'Hollis Hills': 220};
+
+final Map Neighbourhood_group = {'Brooklyn':0, 'Manhattan':1, 'Queens':2, 'Bronx':3, 'Staten Island':4};
+
+final Map Room_type = {'Entire home/apt':0, 'Hotel room':1, 'Private room':2, 'Shared room':3};
+
+final List<double> scale_mean = [0.8814126 ,  38.04257555,  40.72669618, -73.94218229, 0.85186129,  15.19342101,  34.13364965,   1.28683852, 10.43855683, 141.46584721,  10.38375314];
+final List<double> scale_scale = [0.901201322, 43.1751756, 0.0585452690, 0.0570983405,  1.00495316, 16.1729255, 62.2205001, 1.94478734,  37.6716398, 141.179383, 20.6322141];
 
 void main() {
   runApp(const MyApp());
@@ -61,18 +79,19 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> textLabels = [];
   //List<String> textValues;
-  Future<String> predData() async {
+  Future<String> predData(List<double> input) async {
     final interpreter = await Interpreter.fromAsset('converted_model.tflite');
-    var input = [
-      [-0.978042,	-0.834799,	-0.706226,	-0.226586,	1.142480,	0.915516,	0.255002,	-0.507427,	-0.224003,	1.278757,	-0.503279]
-    ];
+    // var input = [
+    //   [-0.978042,	-0.834799,	-0.706226,	-0.226586,	1.142480,	0.915516,	0.255002,	-0.507427,	-0.224003,	1.278757,	-0.503279]
+    // ];
     var output = List.filled(1, 0).reshape([1, 1]);
     interpreter.run(input, output);
     print(output[0][0]);
 
     this.setState(() {
-      predValue = output[0][0].toString();
+      predValue = output[0][0].toStringAsFixed(2);
     });
+    print(predValue);
     return predValue;
   }
   @override
@@ -89,7 +108,7 @@ class _MyHomePageState extends State<MyHomePage> {
       'Reviews per month',
       'Host listing count',
       'Availability(Max:365 Days)',
-      'Textbox 11'
+      'Reviews last month'
     ];
 
 
@@ -178,6 +197,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 2:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -185,6 +205,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 3:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -216,6 +237,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
       case 5:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -223,6 +245,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 6:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -230,6 +253,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 7:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -237,6 +261,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 8:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -244,6 +269,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 9:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -251,6 +277,7 @@ class _MyHomePageState extends State<MyHomePage> {
         );
       case 10:
         return TextField(
+          keyboardType: TextInputType.number,
           controller: _controllers[i],
           decoration: const InputDecoration(
             hintText: 'Enter text here',
@@ -285,7 +312,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     //crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Padding(padding: EdgeInsets.fromLTRB(10, 5, 5, 5)),
-                      Text(textLabels[i] + ' '*(26-textLabels[i].length)+':'),
+                      Text(textLabels[i] + ' '*(20-textLabels[i].length)+':'),
                       SizedBox(width: 25),
                       Expanded(
                         flex: 1,
@@ -310,15 +337,15 @@ class _MyHomePageState extends State<MyHomePage> {
                      for (int i = 0; i < textLabels.length; i++) {
                        if (i==0) {
                          //print(dropdownValue1);
-                         inputList.add(dropdownValue1);
+                         inputList.add(Neighbourhood_group[dropdownValue1].toString());
                        }
                        else if(i==1){
                          //print(dropdownValue2);
-                         inputList.add(dropdownValue2);
+                         inputList.add(Neighbourhood[dropdownValue2].toString());
                        }
                        else if(i==4){
                          //print(dropdownValue3);
-                         inputList.add(dropdownValue3);
+                         inputList.add(Room_type[dropdownValue3].toString());
                        }
                        else{
                         // print(_controllers[i].text);
@@ -331,34 +358,44 @@ class _MyHomePageState extends State<MyHomePage> {
                        }
                      }
                      if((isTextFilled==false) || ((dropdown1&&dropdown2&&dropdown3)==false)){
-                       showDialog(
-                           context: context,
-                           builder: (_) => AlertDialog(
-                             title: Text('Error'),
-                             content: Text('Please fill in all the fields.'),
-                             actions: [
-                               TextButton(
-                                 onPressed: () => Navigator.pop(context),
-                                 child: Text('OK'),
-                               )
-                             ],
-                           )
-                       );
+                       // showDialog(
+                       //     context: context,
+                       //     builder: (_) => AlertDialog(
+                       //       title: Text('Error'),
+                       //       content: Text('Please fill in all the fields.'),
+                       //       actions: [
+                       //         TextButton(
+                       //           onPressed: () => Navigator.pop(context),
+                       //           child: Text('OK'),
+                       //         )
+                       //       ],
+                       //     )
+                       // );
+                       print(predData([2.3508480827550318, 1.5971544641499962, 1.538532857368896, 2.027769791312911, 2.1375510775049458, -0.8776038082905903, -0.5164479488007201, -0.6462601304263941, -0.25054807489426034, 1.547918316019273, -0.5032786636311611]));
+                       // Navigator.push(context,
+                       //         MaterialPageRoute(builder: (context) => MapScreen()));
                        print("ALL NOT FILLED");
                        // print(isTextFilled==false);
                        // print((dropdown1&&dropdown2&&dropdown3)==false);
                        print("INPUT LIST:");
-                       print(inputList);
-                       print(predData());
+                       //print(inputList);
+                       //print(predData());
                      }else{
                        print("ALL FILLED");
                        // print(isTextFilled==false);
                        // print((dropdown1&&dropdown2&&dropdown3)==false);
                        print("INPUT LIST:");
-                       print(inputList);
+                       //print(inputList);
                        // Navigator.push(context,
                        //     MaterialPageRoute(builder: (context) => PredModel()))
-                       print(predData());
+                       List<double> doubleList = inputList.map((str) => double.parse(str)).toList();
+                       List<double> scaledList = [];
+                       for (int i=0;i<11;i++){
+                         scaledList.add((doubleList[i]-scale_mean[i])/scale_scale[i]);
+                       }
+                       print(doubleList);
+                       print(scaledList);
+                       print(predData(scaledList));
                      }
                    },
                   child: Center(
