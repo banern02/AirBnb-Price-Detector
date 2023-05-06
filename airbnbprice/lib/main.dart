b236575b7d1aec1ddf74a59624d0a4ba4c971a2f
Sagnik Mondal <sagnik.mondal.20@gmail.com>
2023-05-06 06:53:33 -0400

    updated predictions

--- airbnbprice/lib/main.dart
+++ airbnbprice/lib/main.dart
@@ -1,15 +1,36 @@
 import 'dart:ffi';
 
+import 'package:airbnbprice/pred_page.dart';
 import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
 
 
 final List<TextEditingController> _controllers = List.generate(11, (index) => TextEditingController());
+
+final List<String> room_list = ['Select an option', 'Entire room/apt', 'Hotel room', 'Private room', 'Shared room'];
+final List<String> Neighbourhood_group_list = ['Select an option', 'Brooklyn', 'Manhattan', 'Queens', 'Bronx', 'Staten Island'];
+final List<String> Neighbourhood_brooklyn = ['Select an option','Bath Beach','Bay Ridge','Bedford-Stuyvesant','Bensonhurst','Bergen Beach','Boerum Hill','Borough Park','Brighton Beach','Brooklyn Heights','Brownsville','Bushwick','Canarsie','Carroll Gardens','Clinton Hill','Cobble Hill','Columbia St','Coney Island','Crown Heights','Cypress Hills','DUMBO','Downtown Brooklyn','Dyker Heights','East Flatbush','East New York','Flatbush','Flatlands','Fort Greene','Fort Hamilton','Gerritsen Beach','Gowanus','Gravesend','Greenpoint','Kensington','Manhattan Beach','Midwood','Mill Basin','Navy Yard','Park Slope','Prospect Heights','Prospect-Lefferts Gardens','Red Hook','Sea Gate','Sheepshead Bay','South Slope','Sunset Park','Vinegar Hill','Williamsburg','Windsor Terrace'];
+final List<String> Neighbourhood_manhattan = ['Select an option','Battery Park City','Chelsea','Chinatown','Civic Center','East Harlem','East Village','Financial District','Flatiron District','Gramercy','Greenwich Village','Harlem','Hell\'s Kitchen','Inwood','Kips Bay','Little Italy','Lower East Side','Marble Hill','Midtown','Morningside Heights','Murray Hill','NoHo','Nolita','Roosevelt Island','SoHo','Stuyvesant Town','Theater District','Tribeca','Two Bridges','Upper East Side','Upper West Side','Washington Heights','West Village'];
+final List<String> Neighbourhood_queens = ['Select an option','Arverne','Astoria','Bay Terrace','Bayside','Bayswater','Belle Harbor','Bellerose','Breezy Point','Briarwood','Cambria Heights','College Point','Corona','Ditmars Steinway','Douglaston','East Elmhurst','Edgemere','Elmhurst','Far Rockaway','Flushing','Forest Hills','Fresh Meadows','Glendale','Hollis','Hollis Hills','Holliswood','Howard Beach','Jackson Heights','Jamaica','Jamaica Estates','Jamaica Hills','Kew Gardens','Kew Gardens Hills','Laurelton','Little Neck','Long Island City','Maspeth','Middle Village','Neponsit','Ozone Park','Queens Village','Rego Park','Richmond Hill','Ridgewood','Rockaway Beach','Rosedale','South Ozone Park','Springfield Gardens','St. Albans','Sunnyside','Whitestone','Woodhaven','Woodside'];
+final List<String> Neighbourhood_bronx = ['Select an option','Allerton','Baychester','Belmont','Bronxdale','Castle Hill','City Island','Claremont Village','Clason Point','Co-op City','Concourse','Concourse Village','Country Club','East Morrisania','Eastchester','Edenwald','Fieldston','Fordham','Highbridge','Hunts Point','Kingsbridge','Longwood','Melrose','Morris Heights','Morris Park','Morrisania','Mott Haven','Mount Eden','Mount Hope','North Riverdale','Norwood','Olinville','Parkchester','Pelham Bay','Pelham Gardens','Port Morris','Riverdale','Schuylerville','Soundview','Spuyten Duyvil','Throgs Neck','Tremont','Unionport','University Heights','Van Nest','Wakefield','West Farms','Westchester Square','Williamsbridge','Woodlawn'];
+final List<String> Neighbourhood_staten = ['Select an option','Arden Heights','Arrochar','Bull\'s Head','Castleton Corners','Chelsea, Staten Island','Clifton','Concord','Dongan Hills','Eltingville','Emerson Hill','Fort Wadsworth','Graniteville','Grant City','Great Kills','Grymes Hill','Howland Hook','Huguenot','Lighthouse Hill','Mariners Harbor','Midland Beach','New Brighton','New Dorp','New Dorp Beach','New Springville','Oakwood','Port Richmond','Prince\'s Bay','Randall Manor','Richmondtown','Rosebank','Shore Acres','Silver Lake','South Beach','St. George','Stapleton','Todt Hill','Tompkinsville','Tottenville','West Brighton','Westerleigh','Willowbrook','Woodrow'];
+List<String> Neighbourhood_dropdown = ['Select an option'];
+var predValue = "";
+
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+List<String> inputList= [];
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+
 
   // This widget is the root of your application.
   @override
@@ -54,12 +75,26 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   List<String> textLabels = [];
   //List<String> textValues;
+  Future<String> predData() async {
+    final interpreter = await Interpreter.fromAsset('converted_model.tflite');
+    var input = [
+      [-0.978042,	-0.834799,	-0.706226,	-0.226586,	1.142480,	0.915516,	0.255002,	-0.507427,	-0.224003,	1.278757,	-0.503279]
+    ];
+    var output = List.filled(1, 0).reshape([1, 1]);
+    interpreter.run(input, output);
+    print(output[0][0]);
+
+    this.setState(() {
+      predValue = output[0][0].toString();
+    });
+    return predValue;
+  }
   @override
   void initState() {
     super.initState();
      textLabels = [
-      'Neighbourhood',
       'Neighbourhood Group',
+      'Neighbourhood',
       'Latitude',
       'Longitude',
       'Room Type',
@@ -85,21 +120,6 @@ class _MyHomePageState extends State<MyHomePage> {
   bool dropdown3 = false;
   bool isTextFilled = false;
 
-  // final List<String> textLabels = [
-  //   'Neighbourhood',
-  //   'Neighbourhood Group',
-  //   'Latitude',
-  //   'Longitude',
-  //   'Room Type',
-  //   'Minimum Nights',
-  //   'Number of reviews',
-  //   'Reviews per month',
-  //   'Host listing count',
-  //   'Availability(Max:365 Days)',
-  //   'Textbox 11'
-  // ];
-
-
 
   Widget _getTextFieldWidget(int i) {
 
@@ -112,14 +132,31 @@ class _MyHomePageState extends State<MyHomePage> {
             setState(() {
               dropdownValue1 = newValue!;
             });
+
             if (dropdownValue1=='Select an option') {
               dropdown1 = false;
             }
             else{
               dropdown1 = true;
             }
+            Neighbourhood_dropdown = ['Select an option'];
+            if (dropdownValue1=='Brooklyn'){
+              Neighbourhood_dropdown = Neighbourhood_brooklyn;
+            }
+            else if (dropdownValue1=='Manhattan'){
+              Neighbourhood_dropdown = Neighbourhood_manhattan;
+            }
+            else if (dropdownValue1=='Queens'){
+              Neighbourhood_dropdown = Neighbourhood_queens;
+            }
+            else if (dropdownValue1=='Bronx'){
+              Neighbourhood_dropdown = Neighbourhood_bronx;
+            }
+            else if (dropdownValue1=='Staten Island'){
+              Neighbourhood_dropdown = Neighbourhood_staten;
+            }
           },
-          items: <String>['Select an option','Option 1', 'Option 2', 'Option 3', 'Option 4']
+          items: Neighbourhood_group_list
               .map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
@@ -130,10 +167,12 @@ class _MyHomePageState extends State<MyHomePage> {
 
       case 1:
         return DropdownButton(
+
           value: dropdownValue2,
           onChanged: (String? newValue) {
             setState(() {
               dropdownValue2 = newValue!;
+
             });
             if (dropdownValue2=='Select an option') {
               dropdown2 = false;
@@ -142,7 +181,8 @@ class _MyHomePageState extends State<MyHomePage> {
               dropdown2 = true;
             }
           },
-          items: <String>['Select an option','Option 1', 'Option 2', 'Option 3', 'Option 4']
+
+          items: Neighbourhood_dropdown
               .map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
@@ -153,14 +193,14 @@ class _MyHomePageState extends State<MyHomePage> {
       case 2:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
       case 3:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
@@ -179,7 +219,7 @@ class _MyHomePageState extends State<MyHomePage> {
               dropdown3 = true;
             }
           },
-          items: <String>['Select an option','Room 1', 'Room 2', 'Room 3', 'Room 4']
+          items: room_list
               .map<DropdownMenuItem<String>>((String value) {
             return DropdownMenuItem<String>(
               value: value,
@@ -191,49 +231,42 @@ class _MyHomePageState extends State<MyHomePage> {
       case 5:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
-            hintText: 'Enter text here',
-          ),
-        );
-      case 7:
-        return TextField(
-          controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
       case 6:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
       case 7:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
       case 8:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
       case 9:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
       case 10:
         return TextField(
           controller: _controllers[i],
-          decoration: InputDecoration(
+          decoration: const InputDecoration(
             hintText: 'Enter text here',
           ),
         );
@@ -265,12 +298,14 @@ class _MyHomePageState extends State<MyHomePage> {
                   Row(
                     //crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Text(textLabels[i] + ':'),
+                      Padding(padding: EdgeInsets.fromLTRB(10, 5, 5, 5)),
+                      Text(textLabels[i] + ' '*(26-textLabels[i].length)+':'),
                       SizedBox(width: 25),
                       Expanded(
                         flex: 1,
                         child: _getTextFieldWidget(i),
                       ),
+                      SizedBox(width: 20),
                     ],
                   ),
                 SizedBox(height: 16),
@@ -284,19 +319,24 @@ class _MyHomePageState extends State<MyHomePage> {
                    onPressed: () {
                      // final List<TextEditingController> _controllers = List.generate(
                      //     textLabels.length, (index) => TextEditingController());
+                     inputList = [];
 
                      for (int i = 0; i < textLabels.length; i++) {
                        if (i==0) {
-                         print(dropdownValue1);
+                         //print(dropdownValue1);
+                         inputList.add(dropdownValue1);
                        }
                        else if(i==1){
-                         print(dropdownValue2);
+                         //print(dropdownValue2);
+                         inputList.add(dropdownValue2);
                        }
                        else if(i==4){
-                         print(dropdownValue3);
+                         //print(dropdownValue3);
+                         inputList.add(dropdownValue3);
                        }
                        else{
-                         print(_controllers[i].text);
+                        // print(_controllers[i].text);
+                         inputList.add(_controllers[i].text);
                          if (_controllers[i].text.isEmpty) {
                            isTextFilled=false;
                          }else{
@@ -319,12 +359,20 @@ class _MyHomePageState extends State<MyHomePage> {
                            )
                        );
                        print("ALL NOT FILLED");
-                       print(isTextFilled==false);
-                       print((dropdown1&&dropdown2&&dropdown3)==false);
+                       // print(isTextFilled==false);
+                       // print((dropdown1&&dropdown2&&dropdown3)==false);
+                       print("INPUT LIST:");
+                       print(inputList);
+                       print(predData());
                      }else{
                        print("ALL FILLED");
-                       print(isTextFilled==false);
-                       print((dropdown1&&dropdown2&&dropdown3)==false);
+                       // print(isTextFilled==false);
+                       // print((dropdown1&&dropdown2&&dropdown3)==false);
+                       print("INPUT LIST:");
+                       print(inputList);
+                       // Navigator.push(context,
+                       //     MaterialPageRoute(builder: (context) => PredModel()))
+                       print(predData());
                      }
                    },
                   child: Center(
@@ -338,5 +386,7 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+
 
 
---
+++ airbnbprice/lib/pred_page.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:tflite_flutter/tflite_flutter.dart';
+
+class PredModel extends StatefulWidget {
+  @override
+  _PredModelState createState() => _PredModelState();
+}
+
+class _PredModelState extends State<PredModel> {
+  var predValue = "";
+  @override
+  void initState() {
+    super.initState();
+    predValue = "click predict button";
+  }
+
+  Future<void> predData() async {
+    final interpreter = await Interpreter.fromAsset('converted_model.tflite');
+    var input = [
+      [1.241218,	0.022175,	-0.568343,	2.402819,	2.137551,	-0.877604,	-0.532520,	-0.147491,	-0.091277,	0.230445,	-0.454811]
+    ];
+    var output = List.filled(1, 0).reshape([1, 1]);
+    interpreter.run(input, output);
+    print(output[0][0]);
+
+    this.setState(() {
+      predValue = output[0][0].toString();
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "change the input values in code to get the prediction",
+              style: TextStyle(fontSize: 16),
+            ),
+            SizedBox(height: 12),
+            MaterialButton(
+              color: Colors.blue,
+              child: Text(
+                "predict",
+                style: TextStyle(fontSize: 25),
+              ),
+              onPressed: predData,
+            ),
+            SizedBox(height: 12),
+            Text(
+              "Predicted value :  $predValue ",
+              style: TextStyle(color: Colors.red, fontSize: 23),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
